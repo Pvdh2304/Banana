1da8157d43e9189183158c38c7baa648f379c452
Phạm Văn Đức Hạnh <137122662+Pvdh2304@users.noreply.github.com>
2024-01-31 20:24:20 +0700

    Update banana.lua

--- banana.lua
+++ banana.lua
@@ -1,4 +1,1 @@
-repeat task.wait() until game:IsLoaded()
-getgenv().Key = "9d7699c4db89adf2"
-getgenv().OldBanana = false
-loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua"))()
+
