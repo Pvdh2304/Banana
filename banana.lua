703ffa051d9c12125ab1e7993f46cacd0864fc2e
Phạm Văn Đức Hạnh <137122662+Pvdh2304@users.noreply.github.com>
2024-01-31 20:23:58 +0700

    Update and rename README.md to banana.lua

---
+++ banana.lua
@@ -0,0 +1,4 @@
+repeat task.wait() until game:IsLoaded()
+getgenv().Key = "9d7699c4db89adf2"
+getgenv().OldBanana = false
+loadstring(game:HttpGet("https://raw.githubusercontent.com/obiiyeuem/vthangsitink/main/BananaHub.lua"))()
