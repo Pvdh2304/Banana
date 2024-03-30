48c2899b4261800e40ecbe5838a912c982af13b3
Phạm Văn Đức Hạnh <137122662+Pvdh2304@users.noreply.github.com>
2024-03-30 20:37:13 +0700

    Update banana perm.lua

--- banana perm.lua
+++ banana perm.lua
@@ -1,18 +1,1 @@
-getgenv().Config = {
-    ["AutoReplay"] = true,
-    ["AutoJoinMatch"] = true,
-    ["SelectMap"] = "Egg Island",
-    ["AutoVoteDifficulty"] = "true",
-    ["SelectDifficulty"] = "Nightmare",
-    ["SelectMacro"] = "loisprite",
-    ["AutoSkip"] = true,
-    ["DeleteMap"] = true,
-    ["PlayMacro"] = true,
-    ["AutoSave"] = true,
-    ["BoostFPS"] = true,
-    ["AutoClaimQuest"] = true,
-    ["WhiteScreen"] = false,
-    ["DelayJoin"] = "2",
-    ["PlaceFailsafe"] = true,
-}
-loadstring(game:HttpGet("https://nousigi.com/loader.lua"))()
+
